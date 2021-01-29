c5283d7dbca12c9c4cf3f17303f925eb2ac7dd8e
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-01-29 17:42:50 +0100

    calculator is done

--- app/javascript/controllers/proposal_controller.js
+++ app/javascript/controllers/proposal_controller.js
@@ -1,18 +1,32 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "salary",'salaryField' ]
+  static targets = [ "salary",'salaryField', "price", "nbmonth" ]
 
   connect() {
     console.log("hello")
   }
 
   change() {
-    console.log(this.salaryTarget.value)
-    this.salaryFieldTarget.value = this.salaryTarget.value
+    if (event.target.dataset.month == undefined) {
+      const numberOfMonth = 12
+      let salary = 45000
+      salary = parseInt(this.salaryTarget.value,10)
+      this.salaryFieldTarget.value = salary
+      this.nbmonthTarget.innerText = numberOfMonth
+      this.priceTarget.innerText = Math.round((salary * 0.16)/ numberOfMonth)
+    } else {
+      const numberOfMonth = event.target.dataset.month
+      let salary = 45000
+      salary = parseInt(this.salaryTarget.value,10)
+      this.salaryFieldTarget.value = salary
+      this.nbmonthTarget.innerText = numberOfMonth
+      this.priceTarget.innerText = Math.round((salary * 0.16)/ numberOfMonth)
+    }
   }
 
   choice() {
-    console.log(event.target.dataset.month)
+    let month = event.target.dataset.month
+    change(month)
   }
 }

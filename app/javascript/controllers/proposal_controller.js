a6f11faf12b3dcb43d9d5c6bf7e50b9305e3d9c0
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-01-29 16:50:32 +0100

    radio buttons done

--- app/javascript/controllers/proposal_controller.js
+++ app/javascript/controllers/proposal_controller.js
@@ -2,6 +2,8 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   static targets = [ "salary",'salaryField' ]
+  static values  = { month: Number }
+
   connect() {
     console.log("hello")
   }
@@ -9,5 +11,12 @@ export default class extends Controller {
   change() {
     console.log(this.salaryTarget.value)
     this.salaryFieldTarget.value = this.salaryTarget.value
+  }
+
+  monthPicked() {
+    console.log('click')
+    console.log(parseInt(event.target.dataset.month,10))
+    event.target.classList.remove('bg-blue-500','text-white')
+
   }
 }

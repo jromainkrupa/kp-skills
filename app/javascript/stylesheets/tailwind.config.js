6d8cd81988eb6f9d8f71dcffb7eb076516651c7a
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-01-31 22:41:48 +0100

    emoved purrg option of tailwind config

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,10 +1,6 @@
 const colors = require('tailwindcss/colors')
 
   module.exports = {
-   purge: [
-     './src/**/*.html',
-     './src/**/*.js',
-   ],
     darkMode: false, // or 'media' or 'class'
     theme: {
       extend: {},

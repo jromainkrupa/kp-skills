57d615aabaa8b585b5c43b1211610114cc30f196
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-01-31 22:57:39 +0100

    added puge tailwind fo pod

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,6 +1,10 @@
 const colors = require('tailwindcss/colors')
 
   module.exports = {
+    purge: [
+     './app/**/*.html.erb',
+     './app/javascript/**/*.js',
+   ],
     darkMode: false, // or 'media' or 'class'
     theme: {
       extend: {},

1330c573894cc330791e682a76458a7f52bafcc6
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-02-01 19:20:41 +0100

    updated with new rates

--- app/javascript/controllers/proposal_controller.js
+++ app/javascript/controllers/proposal_controller.js
@@ -11,15 +11,27 @@ export default class extends Controller {
       salary = parseInt(this.salaryTarget.value,10)
       this.salaryFieldTarget.value = salary
       this.nbmonthTarget.innerText = numberOfMonth
-      this.priceTarget.innerText = Math.round((salary * 0.16)/ numberOfMonth)
+      this.priceTarget.innerText = Math.round((salary * rateCalculator(salary))/ numberOfMonth)
     } else {
       const numberOfMonth = event.target.dataset.month
       let salary = 45000
       salary = parseInt(this.salaryTarget.value,10)
       this.salaryFieldTarget.value = salary
       this.nbmonthTarget.innerText = numberOfMonth
-      this.priceTarget.innerText = Math.round((salary * 0.16)/ numberOfMonth)
+      this.priceTarget.innerText = Math.round((salary * rateCalculator(salary))/ numberOfMonth)
     }
   }
 
 }
+
+  const rateCalculator = (salary) => {
+    if (salary <= 30000) {
+      return 0.14
+    } else if (salary > 30000 && salary <= 60000) {
+      return 0.16
+    } else if (salary > 60000 && salary <= 90000) {
+      return 0.19
+    } else if (salary > 90000) {
+      return 0.2
+    }
+  }

4cb469bfc7c7d4a10be1537b1054765d010c39d5
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-04-01 14:09:14 +0200

    changed home page to select a universe

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,17 +1,29 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
-  module.exports = {
-    purge: [
-     './app/**/*.html.erb',
-     './app/javascript/**/*.js',
-   ],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
+module.exports = {
+  purge: ["./app/**/*.html.erb", "./app/javascript/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        aquamarine: {
+          50: "#f3fffc",
+          100: "#e7fffa",
+          200: "#c3fff2",
+          300: "#9fffea",
+          400: "#58ffdb",
+          500: "#10ffcb",
+          600: "#0ee6b7",
+          700: "#0cbf98",
+          800: "#0a997a",
+          900: "#087d63",
+        },
+      },
     },
-    variants: {},
-    plugins: [
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    ],
-  }
+  },
+  variants: {},
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+  ],
+};
